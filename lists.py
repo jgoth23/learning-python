ed0ac21b30d2af67772fa12fba8e0709c4cc349a
Johnny Goth <Jgoth23@gmail.com>
2021-05-20 15:50:45 -0500

    added lists

---
+++ lists.py
@@ -0,0 +1,8 @@
+# Defines a list of names
+names = ["Larry", "Moe", "Curly"]
+
+names.append("Barry")
+
+names.sort()
+
+print(names)
--- sequences.py
+++ sequences.py
@@ -5,3 +5,9 @@ print(name[0])
 names = ["Ron", "Harry", "Fred"]
 
 print(names[1])
+
+coordinateX = 10.0
+coordinateY = 20.0
+
+coordinate = (10.0, 20.0)
+print (coordinate)
