827962b5eb1030de5faebed382e3b7f8fd3f08c7
Johnny Goth <Jgoth23@gmail.com>
2021-05-20 20:00:04 -0500

    added loops

--- loops.py
+++ loops.py
@@ -1,3 +1,11 @@
 
-for i in [0, 1, 2, 3, 4, 5]:
-  print(i)
+# for i in [0, 1, 2, 3, 4, 5]:
+#   print(i)
+
+for i in range(10):
+    print(i)
+
+names = ["Harry", "Ron", "Herman"]
+
+for name in names:
+  print(name)
