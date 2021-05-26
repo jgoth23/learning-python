51534d25435633ca5bb0175b9961b6b37671c35f
Johnny Goth <Jgoth23@gmail.com>
2021-05-26 11:05:07 -0500

    changed return order

--- lambda.py
+++ lambda.py
@@ -1,11 +1,11 @@
 people = [
-  {"name": "Harry", "House": "Gryffindor"},
-  {"name": "Draco", "House": "Slytherin"},
-  {"name": "Cho", "House": "Ravenclaw"}
+  {"name": "Harry", "house": "Gryffindor"},
+  {"name": "Draco", "house": "Slytherin"},
+  {"name": "Cho", "house": "Ravenclaw"}
 ]
 
 def f(person):
-    return person["name"]
+    return person["house"]
 
 people.sort(key=f)
 print(people)
